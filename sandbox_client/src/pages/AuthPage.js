b666e4a5b19754ee5bdd2a63df916526a0068644
morozov-ak <65528235+morozov-ak@users.noreply.github.com>
2020-05-27 21:51:39 +1000

    add http hook

--- sandbox_client/src/pages/AuthPage.js
+++ sandbox_client/src/pages/AuthPage.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
+import { useHttp } from '../hooks/http.hook'
+
+
 export const AuthPage = () => {
+    const {loading,error,request} = useHttp()
     const[form,setForm]=useState({
         email:'',password:''
     })
@@ -8,9 +12,17 @@ export const AuthPage = () => {
         setForm({...form, [event.target.name]:event.target.value})
         console.log(form)
     }
+
+    const registerHandler = async () => {
+        try{
+            const data = await request('/api/auth/register','POST',{...form})
+            console.log(data)
+        }
+        catch(e){}
+    }
     
     return(
-        
+         
     <div className="auth">
         <div className="form-group">
             <label htmlFor="exampleDropdownFormEmail2">Email address</label>
@@ -22,8 +34,8 @@ export const AuthPage = () => {
             <input name="password" onChange={changeHandler} type="password" className="form-control" id="password" placeholder="Password"/>
         </div>
 
-        <button type="submit" className="btn btn-primary mybtn">Sign in</button>
-        <button type="submit" className="btn btn-success">Register</button>
+        <button type="submit"  className="btn btn-primary mybtn">Sign in</button>
+        <button type="submit" onClick={registerHandler} className="btn btn-success">Register</button>
     </div>  
   
     )
